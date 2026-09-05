cfbf00cca5bd42c025d2edc36f0604060c78158b
rexram987-create <rexram987@gmail.com>
2026-09-05 15:05:05 +0300

    Bump Android app to 0.7.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rexram.plantidentifier"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.7.4"
+        versionCode = 12
+        versionName = "0.7.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
