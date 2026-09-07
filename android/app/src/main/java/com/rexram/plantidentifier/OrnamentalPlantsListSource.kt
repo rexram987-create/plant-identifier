f1899d7e63b562f22c1adb57d03cf5123370de25
rexram987-create <rexram987@gmail.com>
2026-09-07 07:12:37 +0300

    Add official ornamental plants list source metadata

---
+++ android/app/src/main/java/com/rexram/plantidentifier/OrnamentalPlantsListSource.kt
@@ -0,0 +1,10 @@
+package com.rexram.plantidentifier
+
+object OrnamentalPlantsListSource {
+    const val SOURCE_URL = "https://www.gov.il/he/pages/list-of-ornamental-plants-ini-srael"
+
+    const val SUMMARY_HE =
+        "רשימת צמחי הנוי בישראל לשנת 2017 היא מקור רשמי של משרד החקלאות. " +
+        "הרשימה כוללת כ־5,450 מינים וזנים ששימשו בנוי בישראל, עם שמות מדעיים ועבריים ופרטי סיווג. " +
+        "בשלב הראשון היישום מאפשר לפתוח את המקור הרשמי; בשלב הבא נוסיף אינדקס מקומי לחיפוש והתאמה אוטומטית."
+}
